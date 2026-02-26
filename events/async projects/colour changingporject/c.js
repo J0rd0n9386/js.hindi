fa4bdbfc4af444e4a15b5a0b091e9d7c0cf5c8ea
J0rd0n9386 <ayanroy9546@gmail.com>
2026-02-26 15:29:59 +0530

    commit

---
+++ events/async projects/colour changingporject/c.js
@@ -0,0 +1,33 @@
+    //generate a random color
+    
+    const randomColor = function () {
+      const hex = "0123456789ABCDEF";
+      let color = '#';
+    
+      for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+      }
+    
+      return color;
+    };
+    
+    let intervalId = null;
+    
+    const startChangingColor = function () {
+    
+      if (!intervalId) {   // multiple interval prevent
+        intervalId = setInterval(changeBackground, 1000);
+      }
+    
+      function changeBackground() {
+        document.body.style.backgroundColor = randomColor();
+      }
+    };
+    
+    const stopChangingColor = function () {
+      clearInterval(intervalId);
+      intervalId = null;  // reset important
+    };
+    
+    document.querySelector("#start").addEventListener('click', startChangingColor);
+    document.querySelector("#stop").addEventListener('click', stopChangingColor);
